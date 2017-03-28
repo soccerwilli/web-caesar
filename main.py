a152d7e0928a3a5d02cba85ac9e6402edac9f052
Matt Williams <soccerwilli@hotmail.com>
2017-03-27 21:47:51 -0500

    wrap encrypted message in a text area inside a form

--- main.py
+++ main.py
@@ -19,9 +19,13 @@ import caesar
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        message = "Hellooooooo world!"
+        message = 'Hellooooooo world!'
         encrypted_message = caesar.encrypt(message, 13)
-        self.response.write(encrypted_message)
+
+        textarea = "<textarea>" + encrypted_message + "</textarea>"
+        submit = "<input type='submit'/>"
+        form = "<form>" + textarea + "<br>" + submit + "</form>"
+        self.response.write(form)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
